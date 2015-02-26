ef6c7ec981dd6ce7bd865194b701acaa020766b4
Joon Yee Chuah <joonyee.chuah@austinisd.org>
2015-02-26 16:45:32 +0000

    Got support-v4 working. Use default scope in pom.xml. Took me two days to figure out =(

--- java/org/kealinghornets/jchuah/mapsapplication/MapsActivity.java
+++ java/org/kealinghornets/jchuah/mapsapplication/MapsActivity.java
@@ -1,28 +1,32 @@
 package org.kealinghornets.jchuah.mapsapplication;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.app.Activity;
+
+import android.support.v4.app.FragmentActivity;
+/*
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+*/
 public class MapsActivity extends FragmentActivity {
 
+/*
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+//        setUpMapIfNeeded();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+//        setUpMapIfNeeded();
     }
 
     /**
@@ -40,6 +44,7 @@ public class MapsActivity extends FragmentActivity {
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
      * method in {@link #onResume()} to guarantee that it will be called.
      */
+    /*
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -52,14 +57,17 @@ public class MapsActivity extends FragmentActivity {
             }
         }
     }
-
+*/
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
      * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
+    /*
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
+    */
+    
 }
